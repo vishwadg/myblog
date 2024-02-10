fd47728ff22887b12c1299409e9cc80bf452e65f
vishwa <vishwadeep.ghimire@gmail.com>
2024-02-09 22:05:46 -0600

    init: project

---
+++ app/src/main/java/com/example/myblog/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.example.myblog.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+val Teal700 = Color(0xFFD8BB8F)
+val SignUp = Color(0xFF0F2FDF)
+val Black = Color(0xFF111111)
